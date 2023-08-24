2f0737e7c61d596323d6b5c49fdb2965b6a237ed
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2023-08-25 01:17:00 +0200

    Update pid.c

--- pid.c
+++ pid.c
@@ -3,16 +3,20 @@
 
 /**
  * main - Entry point of the program
- # @ac: The number of command line arguments
+ * @ac: The number of command line arguments
  * @av: Array of command line argument strings
+ *
  * Description: This program prints the PID of the parent process.
  *
  * Return: Always 0 (Success)
  */
-
-int main(int ac, char **av) {
+int main(int ac, char **av)
+{
     pid_t parent_pid = getppid();
     printf("Parent PID: %d\n", parent_pid);
-    
-    return 0;
+
+    (void)ac; // Suppress unused parameter warning
+    (void)av; // Suppress unused parameter warning
+
+    return (0);
 }
