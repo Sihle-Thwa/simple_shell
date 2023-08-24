dbd8236fc6150654683ac5db810d7b0c8c5b4ab9
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2023-08-25 01:54:46 +0200

    Update prompt.c

--- prompt.c
+++ prompt.c
@@ -1,63 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define MAX_INPUT_LENGTH 100
-
+#include<stdio.h>
 /**
- * main - Entry point of the program
- * @argv: Array of command line argument strings
- *
- * Description: This program prints all the command line arguments.
- *
- * Return: Always 0 (Success)
- */
-int main(char **argv)
+ * main - prints all arguments without using ac
+ * @ac: number of arguments in av
+ * @av: array of strings (arguments)
+*/
+
+int main(int ac, char **av)
 {
-    int i = 0;
-
-    while (argv[i] != NULL)
+    int i;
+    for(i = 0; i < ac; i++)
     {
-        printf("Argument %d: %s\n", i, argv[i]);
-        i++;
+        (void) ac;
+        printf("%s\n", av[i]);
     }
-
-    return (0);
-}
-
-/**
- * main - Entry point of the program
- *
- * Description: This program displays a prompt, waits for the user to
- *              enter a command, and then prints the entered command.
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-    char input[MAX_INPUT_LENGTH];
-
-    while (1)
-    {
-        printf("$ ");  // Display the prompt
-
-        if (fgets(input, sizeof(input), stdin) == NULL)
-        {
-            if (feof(stdin))  // Handle Ctrl+D (EOF)
-            {
-                printf("\n");
-                break;
-            }
-        }
-
-        input[strcspn(input, "\n")] = '\0';  // Remove newline character
-
-        if (strlen(input) == 0)
-            continue;  // Empty input, prompt again
-
-        printf("You entered: %s\n", input);
-    }
-
     return (0);
 }
