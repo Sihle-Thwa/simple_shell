08cecb368898b485f3aa267a7777a62f4464e2fd
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2023-08-25 01:04:25 +0200

    Update pid.c

--- pid.c
+++ pid.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ * Description: This program prints the PID of the parent process.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main() {
     pid_t parent_pid = getppid();
     printf("Parent PID: %d\n", parent_pid);
